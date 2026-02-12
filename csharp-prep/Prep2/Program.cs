d331c961487e754ed1e344a2715000a880fcff99
Embertheon <whi22060@byui.edu>
2026-02-12 10:22:19 -0700

    Early class proggress

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
+    static int PlusOrMinus(int grade)
+    {
+        if 
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Class score as a percentage? %");
+        string Grade = Console.ReadLine();
+        int GradeValue = int.Parse(Grade);
+        if (GradeValue >= 90)
+        {
+            Console.Write("You have an A");
+        }
+        else if (GradeValue >= 80)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (GradeValue >= 70)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (GradeValue >= 60)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else
+        {
+            Console.WriteLine("You have a F");
+        }
+        if (GradeValue >= 70)
+        {
+            Console.WriteLine("You passed the class congagulations!");
+        }
+
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,5 +1,26 @@
 using System;
+using System.Globalization;
+using System.IO;
+using System.IO.Enumeration;
 
+using (StreamWriter(FileSystemName)) {
+    outpurFile.WriteLine("this will be the first line in the file.");
+
+    string color = "Blue";
+    outputFiile.WriteLine($"My favorite color is {color}");
+}
+DateTime theCurrentTime = DateTime.Now;
+string dateText = theCurrentTime.ToShortDateString();
+
+public class Journal
+{
+    public string
+}
+
+public class Entry
+{
+
+}
 class Program
 {
     static void Main(string[] args)
@@ -7,3 +28,16 @@ class Program
         Console.WriteLine("Hello Develop04 World!");
     }
 }
+
+string filename = "myfile.txt";
+string[] lines = System.IO.File.ReadAllLines(filename);
+
+for each(string line in lines{
+
+    string[] parts = line.Split(',');
+
+    string firstName = parts[0];
+    string lastName = ParallelLoopState[1];
+
+}
+
